fd2ffef1431b4725daad578c9fd7afe252c758b5
evajuma <evansjuma1e@gmailcom>
2026-03-23 22:11:57 +0300

    formatted the file

--- models.go
+++ models.go
@@ -1,25 +1,27 @@
 package main
+
 import "time"
 
 type Account struct {
-	Username string  `json:"username"`
-	Balance  int64 `json:"balance"`
+	Username string `json:"username"`
+	Balance  int64  `json:"balance"`
+	Active   bool   `json:"active"`
 }
 
 type Deposit struct {
-	Username string  `json:"username"`
-	Amount   int64 `json:"amount"`
+	Username string `json:"username"`
+	Amount   int64  `json:"amount"`
 }
 
 type Withdrawal struct {
-	Username string  `json:"username"`
-	Amount   int64 `json:"amount"`
+	Username string `json:"username"`
+	Amount   int64  `json:"amount"`
 }
 
 type Transaction struct {
-	Username string  `json:"username"`
-	Type     string  `json:"type"`
-	Amount   int64 `json:"amount"`
-	Balance  int64 `json:"balance"`
-	Time     time.Time  `json:"time"`
+	Username string    `json:"username"`
+	Type     string    `json:"type"`
+	Amount   int64     `json:"amount"`
+	Balance  int64     `json:"balance"`
+	Time     time.Time `json:"time"`
 }
