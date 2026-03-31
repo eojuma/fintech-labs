e0efee96cbfc6aa696e3e09a97ba2ffaef4a3faf
eojuma <evansjuma1e@gmail.com>
2026-03-31 22:47:36 +0300

    feat: add the user struct because one user can have several accounts under one username

--- backend/models/models.go
+++ backend/models/models.go
@@ -6,9 +6,10 @@ import (
 
 type Account struct {
 	gorm.Model
-	Username string `json:"username" gorm:"uniqueIndex;not null"`
-	Balance  int64  `json:"balance"   gorm:"default:0"`
-	Active   bool   `json:"active"   gorm:"default:true"`
+	UserID  uint   `json:"user_id"`
+	Number  string `json:"number" gorm:"uniqueIndex"`
+	Balance int64  `json:"balance"   gorm:"default:0"`
+	Active  bool   `json:"active"   gorm:"default:true"`
 }
 
 type Deposit struct {
@@ -27,4 +28,12 @@ type Transaction struct {
 	Type     string `json:"type"`
 	Amount   int64  `json:"amount"`
 	Balance  int64  `json:"balance"`
+}
+
+type User struct {
+	gorm.Model
+	Username string    `json:"username" gorm:"uniqueIndex;not null"`
+	Password string    `json:"-" gorm:"not null"`
+	Role     string    `json:"role" gorm:"default:'customer'"`
+	Accounts []Account `json:"accounts" gorm:"foreignKey:UserID"`
 }
