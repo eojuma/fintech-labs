1b5675eb286be4214f44880a1aeba80371971212
eojuma <evansjuma1e@gmail.com>
2026-04-08 02:08:38 +0300

    modify the validator to accept various characters

--- Banking-system/validator/validator.go
+++ Banking-system/validator/validator.go
@@ -5,23 +5,14 @@ import "strings"
 func ValidUsername(username string) bool {
 	username = strings.TrimSpace(username)
 
-	if len(username) < 3 || len(username) > 20 {
+	if len(username) < 4 || len(username) > 64 {
 		return false
 	}
 
 	for _, v := range username {
-
-		if !(v >= 'a' && v <= 'z' || v >= 'A' && v <= 'Z' || v >= '0' && v <= '9') {
+		if !(v >= 'a' && v <= 'z' || v >= 'A' && v <= 'Z' || v >= '0' && v <= '9' || v == '.' || v == '-' || v == '_') {
 			return false
 		}
 	}
-
-	reserved := []string{"admin", "root", "support", "system", "fintech", "official"}
-	for _, word := range reserved {
-		if strings.ToLower(username) == word {
-			return false
-		}
-	}
-
 	return true
 }
