1ecb8a78820a97be63c30d1aebb31528e02be003
eojuma <evansjuma1e@gmail.com>
2026-05-01 16:17:26 +0300

    refactor and feat: refacotr the validity of the username so that it does not allow spaces,added an email validation feature

--- backend/validator/validator.go
+++ backend/validator/validator.go
@@ -1,18 +1,45 @@
 package validator
 
-import "strings"
+import (
+	"regexp"
+	"strings"
+)
 
 func ValidUsername(username string) bool {
-	username = strings.TrimSpace(username)
+	username = strings.ToLower(strings.TrimSpace(username))
 
-	if len(username) < 4 || len(username) > 64 {
+	if len(username) < 3 || len(username) > 30 {
 		return false
 	}
 
 	for _, v := range username {
-		if !(v >= 'a' && v <= 'z' || v >= 'A' && v <= 'Z' || v >= '0' && v <= '9' || v == '.' || v == '-' || v == '_' || v==' ') {
+		if !(v >= 'a' && v <= 'z'|| v >= '0' && v <= '9' || v == '.' || v == '-' || v == '_') {
 			return false
 		}
 	}
 	return true
+}
+
+func ValidFullName(fullname string)bool{
+	fullname=strings.TrimSpace(fullname)
+if len(fullname)<4 || len(fullname)>100{
+	return false
+}
+
+for _,name:=range fullname{
+	if !(name >= 'a' && name <= 'z' || name >= 'A' && name <= 'Z' ||  name==' ') {
+			return false
+		}
+}
+return true
+}
+
+
+func ValidEmail(email string)bool{
+email=strings.TrimSpace(email)
+if len(email) <5 || len(email)>254{
+	return false
+}
+var emailRegex = regexp.MustCompile(`^[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,}$`)
+return emailRegex.MatchString(strings.ToLower(email))
 }
