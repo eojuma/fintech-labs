12e37a0b1d9715ebea8a15b9f60dea6816cd24f7
evajuma <evansjuma1e@gmail.com>
2026-07-17 13:01:42 +0300

    fix(sms): improve transaction SMS message format for better user experience

--- internal/notifications/sms.go
+++ internal/notifications/sms.go
@@ -27,12 +27,29 @@ func SendTransactionSMS(phoneNumber, message string) error {
 }
 
 func FormatSMSMessage(transactionType, accountNumber, referenceNumber string, amount, balance int64) string {
+	var action string
+	switch transactionType {
+	case "deposited":
+		action = fmt.Sprintf("💰 KES %s has been credited to your account", formatAmount(amount))
+	case "withdrawn":
+		action = fmt.Sprintf("💸 KES %s has been debited from your account", formatAmount(amount))
+	case "transferred out":
+		action = fmt.Sprintf("📤 KES %s has been sent from your account", formatAmount(amount))
+	case "received":
+		action = fmt.Sprintf("📥 KES %s has been received in your account", formatAmount(amount))
+	default:
+		action = fmt.Sprintf("KES %s transaction on your account", formatAmount(amount))
+	}
+
 	return fmt.Sprintf(
-		"African Vault: KES %d %s on account %s. New balance: KES %d. Ref: %s. Not you? Contact support immediately.",
-		amount,
-		transactionType,
+		"[African Vault] %s %s. Avail Bal: KES %s. Ref: %s. Not you? Call support immediately. *Charges may apply*",
+		action,
 		accountNumber,
-		balance,
+		formatAmount(balance),
 		referenceNumber,
 	)
+}
+
+func formatAmount(amount int64) string {
+	return fmt.Sprintf("%d", amount)
 }
